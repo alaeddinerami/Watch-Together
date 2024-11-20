072aa1e8713e5dc819fedb44af28bd10994c8690
alaeddinerami <alaeddinerami7@gmail.com>
2024-11-20 14:14:26 +0100

    feat:adding notifiacation , playlist,room

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,11 +3,19 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UsersModule } from './users/users.module';
+import { VideosModule } from './videos/videos.module';
+import { PlaylistsModule } from './playlists/playlists.module';
+import { RoomsModule } from './rooms/rooms.module';
+import { NotificationsModule } from './notifications/notifications.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost:27017/watch-together'),
-    UsersModule
+    UsersModule,
+    VideosModule,
+    PlaylistsModule,
+    RoomsModule,
+    NotificationsModule
   ],
   controllers: [AppController],
   providers: [AppService],
