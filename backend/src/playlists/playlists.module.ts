51bd68e3d5c77a2ecb80e8670e37c573ba592d39
alaeddinerami <alaeddinerami7@gmail.com>
2024-11-21 17:15:18 +0100

    feat:adding module for playlist

--- backend/src/playlists/playlists.module.ts
+++ backend/src/playlists/playlists.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { PlaylistsService } from './playlists.service';
 import { PlaylistsController } from './playlists.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Playlist, PlaylistSchema } from './entities/playlist.entity';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: Playlist.name, schema: PlaylistSchema }]),
+  ],
   controllers: [PlaylistsController],
   providers: [PlaylistsService],
 })
