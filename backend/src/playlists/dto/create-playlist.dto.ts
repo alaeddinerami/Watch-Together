dff7a9bb98b40fda59afa13fa8276231aef63f3b
alaeddinerami <alaeddinerami7@gmail.com>
2024-11-21 17:13:05 +0100

    feat:adding dto playlist

--- backend/src/playlists/dto/create-playlist.dto.ts
+++ backend/src/playlists/dto/create-playlist.dto.ts
@@ -1,1 +1,15 @@
-export class CreatePlaylistDto {}
+import { IsNotEmpty, IsString, IsArray } from 'class-validator';
+import { Types } from 'mongoose';
+
+export class CreatePlaylistDto {
+  @IsNotEmpty()
+  @IsString()
+  title: string;
+
+  @IsNotEmpty()
+  @IsArray()
+  movies: string[];
+
+  @IsNotEmpty()
+  createdBy: Types.ObjectId; 
+}
